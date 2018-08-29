e34013f102c84cd4c1d5c9c1e943939f319d3c7e
github username <github email address>
2018-08-29 17:36:07 -0400

    Defined class that manipulates hash and array.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
 # code here!
+class School
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  attr_accessor :roster
+  attr_reader :name
+
+  def add_student(student, grade)
+    if @roster[grade] == nil
+      @roster[grade] = []
+    end
+    @roster[grade] << student
+  end
+
+  def grade(grade_level)
+    @roster[grade_level]
+  end
+
+  def sort
+    sorted_roster = Hash.new
+    @roster.each do |key, array|
+      sorted_roster[key] = array.sort
+    end
+    sorted_roster
+  end
+end
